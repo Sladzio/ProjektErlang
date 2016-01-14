6e706dd47790262899d21f997c2f5bed92223259
Patryk Sładek <sladzio995@gmail.com>
2016-01-14 19:41:05 +0100

    *Corrected code in example

--- src/countries.erl
+++ src/countries.erl
@@ -12,33 +12,33 @@
 %% API
 -export([generate_hrl/0,run/1]).
 
-
+%%Pomocnicze funkcje do wczytywania plikow
 codeDir() -> filename:dirname(code:which(?MODULE)).
 xsd() -> filename:join([codeDir(), "countries.xsd"]).
 xml()-> filename:join([codeDir(),"countries.xml"]).
+%%Funkcja przeznaczona do generowania naglowka z xsd
 generate_hrl()->
   erlsom:write_xsd_hrl_file("countries.xsd","Countries.hrl").
-run(CountryName)->
+
+%%Glowna funkcjonalnosc
+run(CountryAttr)->
   %% compile xsd
   {ok, Model} = erlsom:compile_xsd_file(xsd()),
   %% parse xml
   {ok, #countries{country = Countries}, _} = erlsom:scan_file(xml(), Model),
-  process(Countries,CountryName).
-  %%lists:foreach(fun process_country/1, Countries).
-process_country(#'countries/country'{code=Code,phoneCode = PhoneCode,name=Name}) ->
-      io:format("Code    : ~s~n"
-      "phoneCode   : ~s~n"
-      "Name: ~s~n",
-        [Code, PhoneCode, Name]).
-process([H|T],CountryName)->
+  display_country(Countries,CountryAttr).
+
+%%Wyswietla rekord zdefiniowany przez CountryName
+display_country([H|T],CountryAttr)->
   case (H#'countries/country'.name) of
-    CountryName-> io:format("Code    : ~s~n"
+    CountryAttr-> io:format("Code    : ~s~n"
     "phoneCode   : ~s~n"
     "Name: ~s~n",
       [H#'countries/country'.code,H#'countries/country'.phoneCode,H#'countries/country'.name]);
-    _->process(T,CountryName)
+
+    _->display_country(T,CountryAttr)
   end;
-process(_,_)->io:format("Nie znaleziono kraju.").
+display_country(_,_)->io:format("Nie znaleziono kraju.").
 
 
 
